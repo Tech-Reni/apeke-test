a912218362a22cc577e768ccd3977e62d60181df
Abraham TireniOluwa Olamide <121473246+Tech-Reni@users.noreply.github.com>
2024-12-14 22:13:32 +0100

    Add files via upload
    Initial Commit, of the new apekesmedia.com website design and update

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Function to show the overlay
+function showOverlay() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('mobile-nav').style.display = 'block';
+}
+
+// Function to hide the overlay
+function hideOverlay() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('mobile-nav').style.display = 'none';
+}
